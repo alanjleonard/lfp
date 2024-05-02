53c852aa26a2a023e6f7c76cbd94295a4431581a
AL Leonard <alanjleonard@protonmail.com>
2024-05-02 14:37:40 -0400

    one and only commit

---
+++ indexmo.js
@@ -0,0 +1,14 @@
+<SCRIPT LANGUAGE="JavaScript">
+image1 = new Image();
+image1.src = "images/elecolor.jpg";
+
+image2 = new Image();
+image2.src = "images/wfcolor.jpg";
+
+image3 = new Image();
+image3.src = "images/othercolor.jpg";
+
+image4 = new Image();
+image4.src = "images/abstcolor.jpg";
+
+</script>
